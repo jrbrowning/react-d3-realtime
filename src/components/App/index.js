6d012155ac87615d3b871c99dd1393f649e15630
Jeff Browning <jeff@craftcoded.com>
2020-03-28 17:58:50 -0400

    working skeleton

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
+import Dashboard from "../Dashboard";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>This is where we begin...</p>
+        <Dashboard />
       </header>
     </div>
   );
---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+
+const Dashboard = () => {
+  return (
+        <p>The next chapter</p>
+  );
+};
+
+export default Dashboard ;
