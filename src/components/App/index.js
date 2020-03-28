21b48f5c162de38d735b5bc85bcf351fa369ced1
Jeff Browning <jeff@craftcoded.com>
2020-03-28 17:50:46 -0400

    initial skeleton framework build out

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is where we begin...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
